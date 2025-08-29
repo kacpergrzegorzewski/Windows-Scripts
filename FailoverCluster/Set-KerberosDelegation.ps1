53e20ca3db046a2abad0a5a5f5261b5d6090b2f3
Kacper Grzegorzewski <kacper-grzegorzewski@wp.pl>
2025-08-29 14:51:01 +0200

    Set-KerberosDelegation

---
+++ FailoverCluster/Set-KerberosDelegation.ps1
@@ -0,0 +1,13 @@
+﻿$Nodes = "node01","node02"
+
+$DomainName = "mydomain.local"
+
+Foreach ($Node in $Nodes){
+        Write-Host "-------- $Node ------"
+        Foreach ($VMHost in $Nodes){
+            If ($Node -notlike $VMHost){
+                Write-Host " -> $VMHost"
+                Get-ADComputer $Node | Set-ADObject -Add @{"msDS-AllowedToDelegateTo"="Microsoft Virtual System Migration Service/$($VMHost).$($DomainName)", "cifs/$($VMHost).$DomainName","Microsoft Virtual System Migration Service/$($VMHost)","cifs/$($VMHost)"}
+        }
+    }
+}
